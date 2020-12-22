d62163cce085d703068228a64d3715df1693ba1b
Denis Craciunescu <dennis.craciunescu@gmail.com>
2020-12-22 06:58:23 +0200

    Remove unused RateService

--- domain/src/main/java/com/deniskrr/domain/RateService.kt
+++
@@ -1,9 +0,0 @@
-package com.deniskrr.domain
-
-import com.deniskrr.domain.model.Currency
-import com.deniskrr.domain.model.RevolutResult
-
-interface RateService {
-
-  suspend fun getExchangeRates(currency: Currency): RevolutResult<Map<String, Double>>
-}
